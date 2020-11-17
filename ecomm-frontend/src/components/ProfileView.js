99ee3337da5a25bcdb4925b008afe1d3e60a0e13
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2020-11-17 18:13:15 +0530

    Make ProfileView.js

---
+++ ecomm-frontend/src/components/ProfileView.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from "react";
+import {useHistory} from "react-router-dom";
+import axios from 'axios';
+const ProfileView = (props) => {
+    const user = localStorage.getItem("user")
+    const history = useHistory()
+    if(!user){
+        history.push("/login")
+    }
+    const [fullProfile, setFullProfile] = useState({})
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_ENDPOINT}/users/`)
+            .then((response)=> {
+                    setFullProfile(response.data)
+                }
+            )
+            .catch((error)=>{
+                localStorage.clear()
+                history.push('/login')
+            })
+    }, [])
+    return(
+        <div>
+            {fullProfile.first_name}
+        </div>
+    )
+}
+
+export default ProfileView;
